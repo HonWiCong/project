6c4caf1f995b046f557919370a467d0fe9ac57b4
HonWiCong <102761891@students.swinburne.edu.my>
2024-05-18 21:50:02 +0800

    .

--- script.py
+++ script.py
@@ -245,7 +245,7 @@ while True:
                         cloudCursor.execute(f"INSERT INTO Cat_Dust_Table (catTableId, dustLevel) VALUES ({newInsertedID}, {dust_level})")
                     
                     cloudDB.commit()
-                elif current_cat_room_pet_number == previous_cat_room_pet_number:
+                elif current_cat_room_pet_number > 0 and current_cat_room_pet_number == previous_cat_room_pet_number:
                     cloudCursor.execute(f"INSERT INTO Cat_Dust_Table (catTableId, dustLevel) VALUES ({newInsertedID}, {dust_level})")
                     cloudDB.commit()
 
