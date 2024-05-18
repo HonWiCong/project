108a5a417a6c813d9d83264e90b9d66f29520b4e
HonWiCong <102761891@students.swinburne.edu.my>
2024-05-18 17:25:00 +0800

    .

--- script.py
+++ script.py
@@ -206,6 +206,7 @@ while True:
                         # Search if the latest record has petCount = 0
                         mycursor.execute(f"SELECT * FROM Cat_Table ORDER BY catTableID DESC LIMIT 1")
                         latest_record = mycursor.fetchone()
+                        print("Latest record:", latest_record)
                         if (latest_record['petCount'] != 0 or not latest_record):
                             sql = "INSERT INTO Cat_Table (petCount, lightState, humidity, temperature_C, temperature_F, dustLevel, windowState, fanState, fanSpeed) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
                             val = (0, light, None, None, None, dust_level, window, fan, fan_speed)
